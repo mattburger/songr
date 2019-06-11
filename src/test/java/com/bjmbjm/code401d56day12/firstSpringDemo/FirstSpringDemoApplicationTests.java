39bc24a8605110b8a433c15182c6c8ccd2f657d7
Matt Burger <matt.burger1@outlook.com>
2019-06-11 16:07:25 -0700

    add .gitignore

---
+++ src/test/java/com/bjmbjm/code401d56day12/firstSpringDemo/FirstSpringDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bjmbjm.code401d56day12.firstSpringDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstSpringDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
